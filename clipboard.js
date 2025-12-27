c84718b1c55ccdd653ddfb144cadeeaf309ca56a
notoiro <notoiro297@gmail.com>
2025-12-27 18:22:24 +0900

    chore: 試験的にEventEmitterを使わないようにしてみる

--- clipboard.js
+++ clipboard.js
@@ -1,13 +1,33 @@
 const clipboard = require('clipboardy').default;
-const EventEmitter = require('events');
 
-module.exports = class Clipboard extends EventEmitter{
+module.exports = class Clipboard{
   constructor(){
-    super();
-
     this.check_interval;
     this.prev;
+    this.listeners = new Map();
   }
+
+  on(event, callback) {
+    if (!this.listeners.has(event)) {
+      this.listeners.set(event, new Set());
+    }
+    this.listeners.get(event).add(callback);
+  }
+
+  off(event, callback) {
+    if (this.listeners.has(event)) {
+      this.listeners.get(event).delete(callback);
+    }
+  }
+
+  emit(event, ...args) {
+    if (this.listeners.has(event)) {
+      for (const cb of this.listeners.get(event)) {
+        cb(...args);
+      }
+    }
+  }
+
   start(){
     this.prev = clipboard.readSync();
     this.check_interval = setInterval(this.check.bind(this), 500);
